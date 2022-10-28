72470ae42c689caccd36ed1051bd83f4b29836f8
ngocbobn <ngocbobn@gmail.com>
2022-10-28 11:40:17 +0700

    get match, upcomint, live

--- backend/app/Services/MatchService.ts
+++ backend/app/Services/MatchService.ts
@@ -20,6 +20,9 @@ export default class MatchService {
     if ('create_match_status' in params) {
       builder = builder.where('create_match_status', params.create_match_status)
     }
+    if ('status' in params) {
+      builder = builder.where('status', params.status)
+    }
     return builder
   }
   async getLiveMatch() {
@@ -46,16 +49,27 @@ export default class MatchService {
   }
 
   public async getListMatch(request): Promise<any> {
-    const page = request.input('page')
-    const size = request.input('size')
+    const page = request.input('page') || 1
+    const size = request.input('size') || 10
+
     if (isNaN(page) || isNaN(size) || parseInt(page) <= 0 || parseInt(size) <= 0)
       throw new InvalidParamException('page or size must be specified as positive number')
     // const MatchModel = require('@ioc:App/Models/Match')
     // let query = MatchModel.query()
     // let listMatch = await query.where('box_id', boxId).where('is_opened', 0).first()
     const Database = require('@ioc:Adonis/Lucid/Database')
-    const matchs = await Database.from('matchs').paginate(page, size)
+    const matchs = await Database.from('matches').paginate(page, size)
 
     return matchs
+  }
+  public async getUpcomingMatch(request) {
+    const page = request.input('page') || 1
+    const size = request.input('size') || 10
+
+    if (isNaN(page) || isNaN(size) || parseInt(page) <= 0 || parseInt(size) <= 0)
+      throw new InvalidParamException('page or size must be specified as positive number')
+    const matches = await this.buildQueryService({ status: 0 }).orderBy('start_time', 'ASC').paginate(page, size)
+
+    return matches
   }
 }
