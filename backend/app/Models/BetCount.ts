b7b172248fcd66298860642698b2fa1d6ef74d72
ngocbobn <ngocbobn@gmail.com>
2022-11-04 17:59:53 +0700

    add field match_status to params for match api

--- backend/app/Models/BetCount.ts
+++ backend/app/Models/BetCount.ts
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
-class BetCount extends BaseModel {
+export default class BetCount extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
