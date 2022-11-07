deabed115b27f44c14dce5391c90fb5e6d3fc469
Nguyễn Khoa <nguyenkhoa@Nguyens-MacBook-Pro.local>
2022-11-07 22:16:12 +0700

    update response predict

--- backend/app/Services/PredictWinnerService.ts
+++ backend/app/Services/PredictWinnerService.ts
@@ -1,12 +1,14 @@
+import InvalidParamException from 'App/Exceptions/InvalidParamException'
 import BusinessException from 'App/Exceptions/BusinessException'
+const HelperUtils = require('@ioc:App/Common/HelperUtils')
 
 export default class PredictWinnerService {
-  public async checkPredictByMatch(request): Promise<any> {
+  public async checkPredictByMatch(request, response): Promise<any> {
     const matchID = request.input('match_id')
     const address = request.input('address')
 
-    if (!address) throw new BusinessException('Wallet address required')
-    if (!matchID) throw new BusinessException('Match ID required')
+    if (!address) return HelperUtils.responseErrorInternal('Wallet address required')
+    if (!matchID) return HelperUtils.responseErrorInternal('Match ID required')
 
     try {
       const PredictModel = require('@ioc:App/Models/Predict')
@@ -15,17 +17,17 @@ export default class PredictWinnerService {
         .andWhere('user_address', address)
         .first()
       if (!predictInfo) {
-        return new BusinessException('User not predict in this match')
+        return HelperUtils.responseErrorInternal('User not predict in this match')
       }
       const MatchModel = require('@ioc:App/Models/Match')
       const matchInfo = await MatchModel.query().where('match_id', matchID).first()
       if (!matchInfo) {
-        return new BusinessException('Match not found')
+        return HelperUtils.responseErrorInternal('Match not found')
       }
       const PredictWinner = require('@ioc:App/Models/PredictWinner')
       const predictWinnerInfo = await PredictWinner.query().where('match_id', matchID).first()
       if (matchInfo?.is_full_time) {
-        return {
+        return HelperUtils.responseSuccess({
           home_score_predict: predictInfo.home_score,
           away_score_predict: predictInfo.away_score,
           predict_time: predictInfo.predict_time,
@@ -33,29 +35,29 @@ export default class PredictWinnerService {
             predictInfo.home_score == matchInfo.ft_home_score &&
             predictInfo.away_score == matchInfo.ft_away_score,
           is_final_winner: predictWinnerInfo ? predictWinnerInfo?.final_winner : null,
-        }
+        })
       } else {
-        return {
+        return HelperUtils.responseSuccess({
           home_score_predict: predictInfo.home_score,
           away_score_predict: predictInfo.away_score,
           predict_time: predictInfo.predict_time,
-        }
+        })
       }
     } catch (error) {
-      throw new BusinessException(error)
+      return HelperUtils.responseErrorInternal(error)
     }
   }
 
   public async getListPredictWinner(request): Promise<any> {
     const matchID = request.input('match_id')
 
-    if (!matchID) throw new BusinessException('Match ID required')
+    if (!matchID) return HelperUtils.responseErrorInternal('Match ID required')
 
     try {
       const MatchModel = require('@ioc:App/Models/Match')
       const matchInfo = await MatchModel.query().where('match_id', matchID).first()
       if (!matchInfo) {
-        return new BusinessException('Match not found')
+        return HelperUtils.responseErrorInternal('Match not found')
       }
       const PredictModel = require('@ioc:App/Models/Predict')
       const predictList = await PredictModel.query()
@@ -64,11 +66,11 @@ export default class PredictWinnerService {
         .andWhere('away_score', matchInfo.ft_away_score)
         .exec()
       if (!predictList || predictList.length === 0) {
-        return new BusinessException('Not predict winner in match')
+        return HelperUtils.responseErrorInternal('Not predict winner in match')
       }
-      return predictList
+      return HelperUtils.responseSuccess(predictList)
     } catch (error) {
-      throw new BusinessException(error)
+      return HelperUtils.responseErrorInternal(error)
     }
   }
 }
