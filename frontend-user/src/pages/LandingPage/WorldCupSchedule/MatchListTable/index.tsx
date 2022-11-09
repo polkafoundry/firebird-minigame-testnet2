8dff3b868a0501c3f43e5f8a41cdb65b5191deeb
King Wisdom <kingwisdom.dev@gmail.com>
2022-11-09 18:50:21 +0700

    Update index.tsx

--- frontend-user/src/pages/LandingPage/WorldCupSchedule/MatchListTable/index.tsx
+++ frontend-user/src/pages/LandingPage/WorldCupSchedule/MatchListTable/index.tsx
@@ -72,108 +72,116 @@ const MatchListTable = (props: MatchListTableProps) => {
       >
         Group stage
       </div>
-      <div className="flex items-center justify-between mt-3">
-        <div className="font-normal text-14/24">(Time Zone: GMT+7)</div>
-        <div className="flex">
-          <div>
-            <span className="text-14/20 font-semibold">Predicted</span>
-            <DropDown
-              label="Predicted"
-              items={predictedOptions}
-              selectedValue={filter.predicted}
-              onChange={handleChangePredicted}
-              className="w-[110px] ml-2 text-14/24"
-              itemsClassName=""
-              bgColor="white"
-            />
-          </div>
-          <div className="ml-3">
-            <span className="text-14/20 font-semibold">Status</span>
-            <DropDown
-              label="Status"
-              items={statusOptions}
-              selectedValue={filter.status}
-              onChange={handleChangeStatus}
-              className="w-[110px] ml-2 text-14/24"
-              itemsClassName=""
-              bgColor="white"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="flex justify-between items-center bg-[#3A0013] text-white mt-1 ">
-        <div
-          className="h-12 w-12 cursor-pointer flex justify-center items-center"
-          onClick={previousGroup}
-        >
-          <img src="/images/icon-previous.svg" alt="" />
-        </div>
-        <div className="text-20/28 font-bold uppercase">
-          {dataTable
-            ? dataTable[groupStageIndex]?.groupRound +
-              " " +
-              dataTable[groupStageIndex]?.date
-            : "N/A"}
-        </div>
-        <div
-          className="h-12 w-12 cursor-pointer flex justify-center items-center"
-          onClick={nextGroup}
-        >
-          <img src="/images/icon-previous.svg" alt="" className="rotate-180" />
-        </div>
-      </div>
-      <div className="overflow-x-auto">
-        <div
-          className={clsx(
-            "flex bg-[#3A0013] min-w-fit text-white",
-            styles.tableRow,
-          )}
-        >
-          {headingTable.map((heading) => (
-            <div key={heading.label} className={heading.className}>
-              {heading.label}
+      <div className="px-5 pt-3 pb-5">
+        <div className="flex items-center justify-between ">
+          <div className="font-normal text-14/24">(Time Zone: GMT+7)</div>
+          <div className="flex">
+            <div>
+              <span className="text-14/20 font-semibold">Predicted</span>
+              <DropDown
+                label="Predicted"
+                items={predictedOptions}
+                selectedValue={filter.predicted}
+                onChange={handleChangePredicted}
+                className="w-[110px] ml-2 text-14/24"
+                itemsClassName=""
+                bgColor="white"
+              />
             </div>
-          ))}
+            <div className="ml-3">
+              <span className="text-14/20 font-semibold">Status</span>
+              <DropDown
+                label="Status"
+                items={statusOptions}
+                selectedValue={filter.status}
+                onChange={handleChangeStatus}
+                className="w-[110px] ml-2 text-14/24"
+                itemsClassName=""
+                bgColor="white"
+              />
+            </div>
+          </div>
         </div>
 
-        {loading ? (
-          <div>Loading ...</div>
-        ) : (
-          dataTable?.map((matchInfo: any, index: number) => (
-            <div key={index} className="min-w-fit">
-              <div className="text-12/18 font-bold px-5 py-[11px] uppercase opacity-80">
-                {matchInfo?.date}
+        <div className="flex justify-between items-center bg-[#3A0013] text-white mt-1">
+          <div
+            className="h-12 w-12 cursor-pointer flex justify-center items-center"
+            onClick={previousGroup}
+          >
+            <img src="/images/icon-previous.svg" alt="" />
+          </div>
+          <div className="text-20/28 font-bold uppercase">
+            {dataTable
+              ? dataTable[groupStageIndex]?.groupRound +
+                " " +
+                dataTable[groupStageIndex]?.date
+              : "N/A"}
+          </div>
+          <div
+            className="h-12 w-12 cursor-pointer flex justify-center items-center"
+            onClick={nextGroup}
+          >
+            <img
+              src="/images/icon-previous.svg"
+              alt=""
+              className="rotate-180"
+            />
+          </div>
+        </div>
+        <div className="overflow-x-auto">
+          <div
+            className={clsx(
+              "flex bg-[#3A0013] min-w-fit text-white",
+              styles.tableRow,
+            )}
+          >
+            {headingTable.map((heading) => (
+              <div key={heading.label} className={heading.className}>
+                {heading.label}
               </div>
-              {matchInfo?.matches?.map((match: any) => (
-                <div
-                  key={match?.id}
-                  className={clsx(
-                    "flex cursor-pointer bg-white hover:bg-orange-300 transition-all duration-300 mb-0.5 last:mb-0",
-                    styles.tableRow,
-                    selectedMatchId === match?.id ? "bg-amber-200" : "",
-                  )}
-                  onClick={() => handleSelectMatch(match?.id)}
-                >
-                  <div className="flex items-center text-14/24">
-                    {getMatchTime(match?.start_time * 1000)}
-                  </div>
-                  <MatchName team1={match?.homeTeam} team2={match?.awayTeam} />
-                  <div className="text-14/24">
-                    {match?.match_status === MATCH_STATUS.UPCOMING
-                      ? "-:-"
-                      : `${match?.ft_home_score}:${match?.ft_away_score}`}
-                  </div>
-                  <MatchStatus status={match?.match_status} />
-                  <MatchPredict
-                    isCorrect={match?.is_completed_bet}
-                    isDisplayText={false}
-                  />
+            ))}
+          </div>
+          {loading ? (
+            <div>Loading ...</div>
+          ) : (
+            dataTable?.map((matchInfo: any, index: number) => (
+              <div key={index} className="min-w-fit">
+                <div className="text-12/18 font-bold px-5 py-[11px] uppercase opacity-80">
+                  {matchInfo?.date}
                 </div>
-              ))}
-            </div>
-          ))
-        )}
+                {matchInfo?.matches?.map((match: any) => (
+                  <div
+                    key={match?.id}
+                    className={clsx(
+                      "flex cursor-pointer bg-white hover:bg-orange-300 transition-all duration-300 mb-0.5 last:mb-0",
+                      styles.tableRow,
+                      selectedMatchId === match?.id ? "bg-amber-200" : "",
+                    )}
+                    onClick={() => handleSelectMatch(match?.id)}
+                  >
+                    <div className="flex items-center text-14/24">
+                      {getMatchTime(match?.start_time * 1000)}
+                    </div>
+                    <MatchName
+                      team1={match?.homeTeam}
+                      team2={match?.awayTeam}
+                    />
+                    <div className="text-14/24">
+                      {match?.match_status === MATCH_STATUS.UPCOMING
+                        ? "-:-"
+                        : `${match?.ft_home_score}:${match?.ft_away_score}`}
+                    </div>
+                    <MatchStatus status={match?.match_status} />
+                    <MatchPredict
+                      isCorrect={match?.is_completed_bet}
+                      isDisplayText={false}
+                    />
+                  </div>
+                ))}
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
