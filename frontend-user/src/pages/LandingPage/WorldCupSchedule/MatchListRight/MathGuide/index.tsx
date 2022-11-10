0a211aa6cc8318220150e5523048257b2998a79f
King Wisdom <kingwisdom.dev@gmail.com>
2022-11-10 14:23:46 +0700

    fe: update UI for match guide section

--- frontend-user/src/pages/LandingPage/WorldCupSchedule/MatchListRight/MathGuide/index.tsx
+++ frontend-user/src/pages/LandingPage/WorldCupSchedule/MatchListRight/MathGuide/index.tsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import { useContext } from "react";
-import Button from "../../../../../components/base/Button";
 import { quickGuide } from "../../../../../constants";
 import { WalletContext } from "../../../../../context/WalletContext";
 import { useMyWeb3 } from "../../../../../hooks/useMyWeb3";
@@ -31,136 +30,142 @@ const MatchGuide = (props: MatchGuideProps) => {
       <div className="flex flex-col">
         <span className={styles.title}>Conditions to join</span>
         <div className="py-3 px-8 font-inter">
-          <ul className="flex flex-col gap-3 text-16/24 bg-white p-5">
-            <li className="flex justify-between">
-              <div className="flex items-center gap-2">
-                <img
-                  className="w-[14px] h-[14px]"
-                  src={account ? iconCheck : iconUnCheck}
-                  alt=""
-                />
-                <span>Connet Wallet</span>
-              </div>
-              {account ? (
-                <p className="m-0 font-semibold font-tthoves">
-                  {displayWalletAddress(account)}
-                </p>
-              ) : (
-                <div
-                  className={clsx(
-                    "h-auto cursor-pointer font-semibold text-sm",
-                    isDetailGuide && buttonStyles,
-                  )}
+          <div className="bg-white p-5">
+            {!account && (
+              <div className="h-[176px] flex justify-center items-center">
+                <button
+                  className="py-2 px-9 rounded-lg bg-[#EB522F] text-white justify-center font-semibold font-tthoves text-14/20"
                   onClick={() => setShowModal && setShowModal(true)}
                 >
-                  Connect wallet
-                </div>
-              )}
-            </li>
-            <li className="flex justify-between">
-              <div className="flex items-center gap-2">
-                <img
-                  className="w-[14px] h-[14px]"
-                  src={predictConditions.network ? iconCheck : iconUnCheck}
-                  alt=""
-                />
-                <div className="flex flex-col">
-                  <span>Network</span>
-                  {isDetailGuide && !predictConditions.network && (
-                    <p className={textError}>You must use Firefly testnet.</p>
-                  )}
-                </div>
+                  Connect Wallet
+                </button>
               </div>
-              {predictConditions.network ? (
-                <p className="m-0 font-semibold font-tthoves">
-                  Firefly Testnet
-                </p>
-              ) : (
-                <div>
-                  <div
-                    className={clsx(
-                      "h-auto cursor-pointer font-semibold text-sm",
-                      isDetailGuide && buttonStyles,
-                    )}
-                    onClick={() => requestSupportNetwork()}
-                  >
-                    Switch Network
+            )}
+            {account && (
+              <ul className="flex flex-col gap-3 text-16/24">
+                <li className="flex justify-between">
+                  <div className="flex items-center gap-2">
+                    <img
+                      className="w-[14px] h-[14px]"
+                      src={account ? iconCheck : iconUnCheck}
+                      alt=""
+                    />
+                    <span>Connet Wallet</span>
                   </div>
-                </div>
-              )}
-            </li>
-            <li className="flex justify-between">
-              <div className="flex items-center gap-2">
-                <img
-                  className="w-[14px] h-[14px]"
-                  src={predictConditions.birdToken ? iconCheck : iconUnCheck}
-                  alt=""
-                />
-                <div className="flex flex-col">
-                  <span>$BIRD to deposit</span>
-                  {isDetailGuide && !predictConditions.birdToken && (
-                    <p className={textError}>
-                      $BIRD is our testnet token. You need to deposit $BIRD to
-                      anwser the questions.
+                  <p className="m-0 font-semibold font-tthoves">
+                    {displayWalletAddress(account)}
+                  </p>
+                </li>
+                <li className="flex justify-between">
+                  <div className="flex items-baseline gap-2">
+                    <img
+                      className="w-[14px] h-[14px]"
+                      src={predictConditions.network ? iconCheck : iconUnCheck}
+                      alt=""
+                    />
+                    <div className="flex flex-col">
+                      <span>Network</span>
+                      {isDetailGuide && !predictConditions.network && (
+                        <p className={textError}>
+                          You must use Firefly testnet.
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  {predictConditions.network ? (
+                    <p className="m-0 font-semibold font-tthoves">
+                      Firefly Testnet
                     </p>
+                  ) : (
+                    <div>
+                      <div
+                        className={clsx(
+                          "h-auto cursor-pointer font-semibold text-sm",
+                          isDetailGuide && buttonStyles,
+                        )}
+                        onClick={() => requestSupportNetwork()}
+                      >
+                        Switch Network
+                      </div>
+                    </div>
                   )}
-                </div>
-              </div>
-              {predictConditions.birdToken ? (
-                <p className="m-0 font-semibold font-tthoves">{`${
-                  birdBalance || 0
-                } $BIRD`}</p>
-              ) : (
-                <div className="flex flex-col">
-                  <a
-                    href="https://faucet.firefly.firebirdchain.com/"
-                    target={"_blank"}
-                    rel="norefferer"
-                    className={clsx(
-                      "h-auto cursor-pointer font-semibold text-sm",
-                      isDetailGuide && buttonStyles,
-                    )}
-                  >
-                    Faucet $BIRD
-                  </a>
-                </div>
-              )}
-            </li>
-            <li className="flex justify-between">
-              <div className="flex items-center gap-2">
-                <img
-                  className="w-[14px] h-[14px]"
-                  src={predictConditions.gasFee ? iconCheck : iconUnCheck}
-                  alt=""
-                />
-                <div className="flex flex-col">
-                  <span>Gas fee</span>
-                  {isDetailGuide && !predictConditions.gasFee && (
-                    <p className={textError}>
-                      You need $PKF to pay for the gas fee.
-                    </p>
+                </li>
+                <li className="flex justify-between">
+                  <div className="flex items-baseline gap-2">
+                    <img
+                      className="w-[14px] h-[14px]"
+                      src={
+                        predictConditions.birdToken ? iconCheck : iconUnCheck
+                      }
+                      alt=""
+                    />
+                    <div className="flex flex-col">
+                      <span>$BIRD to deposit</span>
+                      {isDetailGuide && !predictConditions.birdToken && (
+                        <p className={textError}>
+                          $BIRD is our testnet token. You need to deposit $BIRD
+                          to anwser the questions.
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  {predictConditions.birdToken ? (
+                    <p className="m-0 font-semibold font-tthoves">{`${
+                      birdBalance || 0
+                    } $BIRD`}</p>
+                  ) : (
+                    <div className="flex flex-col">
+                      <a
+                        href="https://faucet.firefly.firebirdchain.com/"
+                        target={"_blank"}
+                        rel="norefferer"
+                        className={clsx(
+                          "h-auto cursor-pointer font-semibold text-sm",
+                          isDetailGuide && buttonStyles,
+                        )}
+                      >
+                        Faucet $BIRD
+                      </a>
+                    </div>
                   )}
-                </div>
-              </div>
-              {predictConditions.gasFee ? (
-                <p className="m-0 font-semibold font-tthoves">{`${realTimeBalance} $${nativeCurrency}`}</p>
-              ) : (
-                <div className="flex flex-col">
-                  <a
-                    href="https://faucet.firefly.firebirdchain.com/"
-                    target={"_blank"}
-                    rel="norefferer"
-                    className={clsx(
-                      "h-auto font-semibold text-sm",
-                      isDetailGuide && buttonStyles,
-                    )}
-                  >
-                    Faucet $PKF
-                  </a>
-                </div>
-              )}
-            </li>
-          </ul>
+                </li>
+                <li className="flex justify-between">
+                  <div className="flex items-baseline gap-2">
+                    <img
+                      className="w-[14px] h-[14px]"
+                      src={predictConditions.gasFee ? iconCheck : iconUnCheck}
+                      alt=""
+                    />
+                    <div className="flex flex-col">
+                      <span>Gas fee</span>
+                      {isDetailGuide && !predictConditions.gasFee && (
+                        <p className={textError}>
+                          You need $PKF to pay for the gas fee.
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  {predictConditions.gasFee ? (
+                    <p className="m-0 font-semibold font-tthoves">{`${realTimeBalance} $${nativeCurrency}`}</p>
+                  ) : (
+                    <div className="flex flex-col">
+                      <a
+                        href="https://faucet.firefly.firebirdchain.com/"
+                        target={"_blank"}
+                        rel="norefferer"
+                        className={clsx(
+                          "h-auto font-semibold text-sm",
+                          isDetailGuide && buttonStyles,
+                        )}
+                      >
+                        Faucet $PKF
+                      </a>
+                    </div>
+                  )}
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
       </div>
 
