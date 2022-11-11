bceb430a69ce836e70e211ef3187f964ebd7b30f
ngocbobn <ngocbobn@gmail.com>
2022-11-11 21:38:13 +0700

    fix query leaderboard data: offer params

--- backend/app/Controllers/Http/MetaForceController.ts
+++ backend/app/Controllers/Http/MetaForceController.ts
@@ -10,7 +10,7 @@ export default class MetaForcesController {
             const wallet_address = request.input('wallet_address')
             const currentTime = new Date()
             const token = HelperUtils.getMFToken(`${currentTime.getTime()}${Const.MF_KEY.TENANT_ID}${Const.MF_KEY.SECRET_KEY}`)
-            const res = await axios.get(`${Const.MF_ENDPOINT}/firebird/query/dev/dashboard/${currentTime.getTime()}?startTime=${params.startTime}&endTime=${params.endTime}&event=${Const.MF_KEY.EVENT_NAME}&tenantId=${Const.MF_KEY.TENANT_ID}&sumBy=earned&limit=${params.limit}&offset=${params.limit}` + (wallet_address ? `&currentUserId=${wallet_address}` : ''), {
+            const res = await axios.get(`${Const.MF_ENDPOINT}/firebird/query/dev/dashboard/${currentTime.getTime()}?startTime=${params.startTime}&endTime=${params.endTime}&event=${Const.MF_KEY.EVENT_NAME}&tenantId=${Const.MF_KEY.TENANT_ID}&sumBy=earned&limit=${params.limit}&offset=${params.offset}` + (wallet_address ? `&currentUserId=${wallet_address}` : ''), {
                 headers: {
                     "Authorization": `Bearer ${token}`
                 }
