659052ac34fdd5377f65632dce7a68553f48c7b9
Ha Nguyen <38517373+theforcee@users.noreply.github.com>
2022-11-16 09:59:44 +0700

    FE: fix current rank

--- frontend-user/src/pages/MyHistoryPage/Statistic/index.tsx
+++ frontend-user/src/pages/MyHistoryPage/Statistic/index.tsx
@@ -7,42 +7,42 @@ const boxStyles = {
 };
 
 const Statistics = (props: any) => {
-  const { data, navActived } = props;
+  const { data, navActived, currentRank } = props;
 
   return (
     <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 justify-center items-center gap-4 mt-4 md:mt-6">
       <div className={boxStyles.box}>
         <span className={boxStyles.title}>Prediction times</span>
-        <span className={boxStyles.number}>{data.prediction_times}</span>
+        <span className={boxStyles.number}>{data?.prediction_times || 0}</span>
       </div>
       <div className={boxStyles.box}>
         <span className={boxStyles.title}>Correct answer</span>
-        <span className={boxStyles.number}>{data.correct_answers}</span>
+        <span className={boxStyles.number}>{data?.correct_answers || 0}</span>
       </div>
       <div className={boxStyles.box}>
         <span className={boxStyles.title}>Win rate</span>
-        <span className={boxStyles.number}>{data.win_rate}</span>
+        <span className={boxStyles.number}>{data?.win_rate || 0}</span>
       </div>
       {navActived === HISTORY_NAV_VALUES.GOALS ? (
         <>
           <div className={boxStyles.box}>
             <span className={boxStyles.title}>Total $BIRD earned</span>
-            <span className={boxStyles.number}>{data.earned}</span>
+            <span className={boxStyles.number}>{data?.earned || 0}</span>
           </div>
           <div className={boxStyles.box}>
             <span className={boxStyles.title}>Current rank</span>
-            <span className={boxStyles.number}>#{data.current_rank}</span>
+            <span className={boxStyles.number}>#{currentRank || 0}</span>
           </div>
         </>
       ) : (
         <>
           <div className={boxStyles.box}>
             <span className={boxStyles.title}>Win whitelist times</span>
-            <span className={boxStyles.number}>{data.win_whitelist}</span>
+            <span className={boxStyles.number}>{data?.win_whitelist || 0}</span>
           </div>
           <div className={boxStyles.box}>
             <span className={boxStyles.title}>Total $ earned</span>
-            <span className={boxStyles.number}>{data.earned}</span>
+            <span className={boxStyles.number}>{data?.earned || 0}</span>
           </div>
         </>
       )}
