bd3898c9bf1826ca60973b6160263c2c2b401c3a
Ha Nguyen <38517373+theforcee@users.noreply.github.com>
2022-11-17 11:32:28 +0700

    FE: update aos config

--- frontend-user/src/App.tsx
+++ frontend-user/src/App.tsx
@@ -1,13 +1,10 @@
-import "./App.css";
-import { Web3ReactProvider } from "@web3-react/core";
-import createRoutes from "./routes";
 import { Web3Provider } from "@ethersproject/providers";
+import { Web3ReactProvider } from "@web3-react/core";
 import { ethers } from "ethers";
-import WalletProvider from "./context/WalletProvider";
+import "./App.css";
 import AppProvider from "./context/AppProvider";
-import AOS from "aos";
-import { useEffect } from "react";
-import "aos/dist/aos.css";
+import WalletProvider from "./context/WalletProvider";
+import createRoutes from "./routes";
 
 export const getLibrary = (provider: any): Web3Provider => {
   const library = new ethers.providers.Web3Provider(provider, "any");
@@ -16,14 +13,6 @@ export const getLibrary = (provider: any): Web3Provider => {
 };
 
 function App() {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      disable: "mobile",
-    });
-    AOS.refresh();
-  }, []);
-
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <WalletProvider>
