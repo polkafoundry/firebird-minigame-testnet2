131acdb439db7967be5b589f6afb681563401b39
Ha Nguyen <hnn152151927@gmail.com>
2022-11-19 23:36:22 +0700

    FE: leaderboard mobile

--- frontend-user/src/pages/LeaderboardPage/RewardBanner/index.tsx
+++ frontend-user/src/pages/LeaderboardPage/RewardBanner/index.tsx
@@ -20,17 +20,17 @@ const RewardBanner = (props: RewardBannerProps) => {
     <div className="mt-2 flex flex-col items-center">
       <div
         className={clsx(
-          "flex items-center font-tthoves border border-black",
+          "flex flex-col xs:flex-row items-center font-tthoves border border-black",
           className,
         )}
       >
-        <div className="w-[340px] flex flex-col items-center px-[70px] py-[14px] border-r border-black">
+        <div className="w-[340px] flex flex-col items-center px-[70px] py-[14px] border-r-0 xs:border-r border-black">
           <span className="text-16/20 font-semibold uppercase">
             Total Reward
           </span>
           <span className="text-48/60 font-tthovesBold italic">{reward}</span>
         </div>
-        <div className="w-[340px] flex flex-col items-center px-[70px] py-[14px]">
+        <div className="w-[340px] flex flex-col items-center px-[70px] py-[14px] border-t border-black xs:border-t-0">
           <span className="text-16/20 font-semibold text-center uppercase">
             Total Winners
           </span>
