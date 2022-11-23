7d9fa6a7871f98c9de15b00487b6d7d8a0631c1b
Nguyễn Khoa <nguyenkhoa@Nguyens-MacBook-Pro.local>
2022-11-23 23:08:24 +0700

    udpate try catch CalcPredictJob

--- backend/app/Jobs/CalcPredictJob.ts
+++ backend/app/Jobs/CalcPredictJob.ts
@@ -6,28 +6,32 @@ export default class CalcPredictJob implements JobContract {
   public key = 'CalcPredictJob'
 
   public async handle() {
-    let match = await MatchModel.query()
-      .where('is_full_time', true)
-      .andWhere('is_pick_predict_final_winners', false)
-      .first()
+    try {
+      let match = await MatchModel.query()
+        .where('is_full_time', true)
+        .andWhere('is_pick_predict_final_winners', false)
+        .first()
 
-    if (!match) {
-      return
-    }
+      if (!match) {
+        return
+      }
 
-    let predict = await PredictModel.query().where('match_predicted', false).first()
-    if (!predict) {
-      return
+      let predict = await PredictModel.query().where('match_predicted', false).first()
+      if (!predict) {
+        return
+      }
+      await PredictModel.query()
+        .where('match_id', match.match_id)
+        .where('home_score', match.ft_home_score)
+        .where('away_score', match.ft_away_score)
+        .where('match_predicted', false)
+        .update({ result: true, match_predicted: true })
+      await PredictModel.query()
+        .where('match_id', match.match_id)
+        .where('match_predicted', false)
+        .update({ result: false, match_predicted: true })
+    } catch (error) {
+      console.log('CalcPredictJob', error)
     }
-    await PredictModel.query()
-      .where('match_id', match.match_id)
-      .where('home_score', match.ft_home_score)
-      .where('away_score', match.ft_away_score)
-      .where('match_predicted', false)
-      .update({ result: true, match_predicted: true })
-    await PredictModel.query()
-      .where('match_id', match.match_id)
-      .where('match_predicted', false)
-      .update({ result: false, match_predicted: true })
   }
 }
